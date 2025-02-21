49cf5416f88f5fa580d53236003aee3ddbdc8960
savi3karanth <Karanth.savi3@gmail.com>
2025-02-20 19:57:34 -0500

    created the methods for an object using the class

---
+++ Adding_methods_class.py
@@ -0,0 +1,21 @@
+class User:
+
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
+user_1 = User("001", "savithri")
+user_2 = User("002", "gayathri")
+
+user_1.follow(user_2)
+print("user_1.followers:", user_1.followers)
+print("user_1.following:", user_1.following)
+print("user_2.followers:", user_2.followers)
+print("user_2.following:", user_2.following)
